ae83c292755926913a265b0f39e83bd0a39fc021
rtnkanth <rtnkanth@gmail.com>
2020-11-05 08:37:48 +0530

    Pet Clinic Project for Spring Boot

---
+++ src/main/java/rtnk/springframework/modpetclinic/ModpetclinicApplication.java
@@ -0,0 +1,13 @@
+package rtnk.springframework.modpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModpetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModpetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rtnk/springframework/modpetclinic/ModpetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package rtnk.springframework.modpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModpetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
