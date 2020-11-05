ddf00272f691e5285a763411c8a850c0fcbc4ab7
rtnkanth <rtnkanth@gmail.com>
2020-11-05 19:39:59 +0530

    Maven Release Plugin

---
+++ pet-clinic-web/src/test/java/rtnk/springframework/modpetclinic/ModPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package rtnk.springframework.modpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class ModPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
