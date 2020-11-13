1d5508b8402248adb57d453d0522ee93c4373653
rtnkanth <rtnkanth@gmail.com>
2020-11-13 23:38:06 +0530

    Added Specialities Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/rtnk/springframework/modpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package rtnk.springframework.modpetclinic.services;
+
+import rtnk.springframework.modpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/rtnk/springframework/modpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,36 @@
+package rtnk.springframework.modpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import rtnk.springframework.modpetclinic.model.Speciality;
+import rtnk.springframework.modpetclinic.services.SpecialitiesService;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
