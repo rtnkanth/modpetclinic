9e8eaf15374b494df87f7a938c4060d0824acf6d
rtnkanth <rtnkanth@gmail.com>
2020-11-13 16:15:34 +0530

    Added PetType Service. closes #29

---
+++ pet-clinic-data/src/main/java/rtnk/springframework/modpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package rtnk.springframework.modpetclinic.services;
+
+import rtnk.springframework.modpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService <PetType, Long>{
+}
